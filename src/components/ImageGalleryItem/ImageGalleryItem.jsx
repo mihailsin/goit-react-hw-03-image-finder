fcd7e04ff1c451b7b5410e67f6b7dacaa7b095fc
mihailsin <mihailsin88@gmail.com>
2022-01-21 00:51:56 +0200

    GET request refactored with async/await syntax

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -4,21 +4,31 @@ import fetchPictures from '../../services/';
 class ImageGalleryItem extends Component {
   state = {
     response: null,
+    isLoading: false,
+    error: null,
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
-      fetchPictures(this.props.searchQuery).then(response =>
-        this.setState({ response }),
-      );
+      this.setState({ isLoading: true });
+      try {
+        const response = await fetchPictures(this.props.searchQuery);
+        this.setState({ response });
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
   }
 
   render() {
     const { response } = this.state;
+    console.log(this.state.response);
 
     if (response) {
       const pictures = response.data.hits;
+      console.log(pictures);
 
       return pictures.map(({ id, previewURL, tags }) => (
         <li key={id} className="gallery-item">
--- src/services/pixabayApi.js
+++ src/services/pixabayApi.js
@@ -5,12 +5,12 @@ const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
 // const REQUEST_PARAMS =
 //   'image_type=photo&orientation=horizontal&safesearch=true';
 
-const fetchPictures = searchQuery =>
-  axios
-    .get(`https://pixabay.com/api/?key=${KEY}&q=${searchQuery}`)
-    .then(response => {
-      console.log(response);
-      return response;
-    });
+const fetchPictures = async searchQuery => {
+  const response = await axios.get(
+    `https://pixabay.com/api/?key=${KEY}&q=${searchQuery}`,
+  );
+  console.log(response);
+  return response;
+};
 
 export default fetchPictures;
